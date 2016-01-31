27b4c9c34c9b1fa9d23b79fc346051283e7d1f1d
Guillermo Varela <guillermo.varela@mobiliti.me>
2016-01-31 16:28:21 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# gulp-demo
+Demonstration on how to use Gulp in a web project.
+
+- Just run from the root folder:
+ - npm install
+ - gulp compress
+- Then open "dist/index.html" in your web browser.
+
+A more detailed tutorial can be found (in spanish) at: http://nombre-temp.blogspot.com/2016/01/tutorial-sobre-gulpjs-automatizando.html
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp   = require('gulp');
+var inject = require('gulp-inject');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var uglify = require('gulp-uglify');
+var gutil  = require('gulp-util');
+var cssnano = require('gulp-cssnano');
+
+// Search for js and css files created for injection in index.html
+gulp.task('inject', function () {
+  return gulp.src('index.html', {cwd: './app'})
+    .pipe(inject(
+      gulp.src('**/*.js', {cwd: './app'}), {
+        read: false,
+        relative: true
+      }))
+    .pipe(inject(
+      gulp.src('**/*.css', {cwd: './app'}), {
+        read: false,
+        relative: true
+      }))
+    .pipe(gulp.dest('./app'));
+});
+
+// Compress into a single file the ones in between of blocks "build:xx" in index.html
+gulp.task('compress', ['inject'], function () {
+  return gulp.src('index.html', {cwd: './app'})
+    .pipe(useref())
+    .pipe(gulpIf('**/*.js', uglify({
+      mangle: true
+    }).on('error', gutil.log)))
+    .pipe(gulpIf('**/*.css', cssnano()))
+    .pipe(gulp.dest('./dist'));
+});
+
+// Copies the assets into the dist folder
+gulp.task('copy:assets', function () {
+  return gulp.src('assets*/**', {cwd: './app'})
+    .pipe(gulp.dest('./dist'));
+});
